4553a9b8a6c23c1d6137050cfd18e3ed0a969ed9
Edouard Justin <justin.edouard4@etu.univ-lorraine.fr>
2025-05-22 08:40:55 +0200

    Ajout du Main

--- SAE_S2-02/src/Main.java
+++ SAE_S2-02/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        GrapheListe gl = new GrapheListe();
+        gl.ajouterArc("a","b",12);
+        gl.ajouterArc("a","d",87);
+        gl.ajouterArc("b","e",11);
+        gl.ajouterArc("c","a",19);
+        gl.ajouterArc("d","b",23);
+        gl.ajouterArc("d","c",10);
+        gl.ajouterArc("e","d",43);
     }
 }
