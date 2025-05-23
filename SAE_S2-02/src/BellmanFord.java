e6284bfd8906ff9d64dfb74493c6c2bcc35dfeae
Kenan KEISER <kenan.keiser2@etu.univ-lorraine.fr>
2025-05-23 08:12:27 +0200

    premier commit J2

---
+++ SAE_S2-02/src/BellmanFord.java
@@ -0,0 +1,44 @@
+public class BellmanFord {
+
+    /**
+     * Algo Point Fixe
+     *
+     * fonction pointFixe(G : graphe, depart : chaîne) : rien
+     *     début
+     *         pour chaque X dans G faire
+     *             L[X] ← +∞
+     *             parent[X] ← ""
+     *         fpour
+     *
+     *         L[depart] ← 0
+     *         parent[depart] ← ""
+     *
+     *         modifie ← vrai
+     *
+     *         tant que modifie = vrai faire
+     *             modifie ← faux
+     *
+     *             pour chaque X dans G faire
+     *                 voisins ← voisins de X dans G
+     *
+     *                 pour chaque N dans voisins faire
+     *                     coutArc ← coût de l'arc de X à N dans G
+     *                     nouvelleValeur ← L[X] + coutArc
+     *
+     *                     si nouvelleValeur < L[N] alors
+     *                         L[N] ← nouvelleValeur
+     *                         parent[N] ← X
+     *                         modifie ← vrai
+     *                     fsi
+     *                 fpour
+     *             fpour
+     *         ftant
+     *     fin
+     */
+
+    public Valeurs resoudre(Graphe g, String depart) {
+        
+    }
+
+
+}
