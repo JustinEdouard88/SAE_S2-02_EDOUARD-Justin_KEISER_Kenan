63395fb10f780b86f7a6b580cd12ee975c057e79
Kenan KEISER <kenan.keiser2@etu.univ-lorraine.fr>
2025-05-23 08:42:15 +0200

    Fonctionne BellmanFord stp

--- SAE_S2-02/src/BellmanFord.java
+++ SAE_S2-02/src/BellmanFord.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BellmanFord {
 
     /**
@@ -34,11 +37,46 @@ public class BellmanFord {
      *             fpour
      *         ftant
      *     fin
+     *
+     * Lexique:
+     * G: graphe orienté
+     * X: chaîne
+     * N: chaîne
+     * depart: chaîne
+     * L: table de réels (associée à chaque nœud)
+     * parent: table de chaînes (associée à chaque nœud)
+     * voisins: tableau de chaînes
+     * nouvelleValeur: réel
+     * coutArc: réel
+     * modifie: booléen
+     *
      */
 
     public Valeurs resoudre(Graphe g, String depart) {
-        
+        Valeurs valeurs = new Valeurs();
+        for(int x = 0; x<g.listeNoeud().size(); x++) {
+            valeurs.setValeur(g.listeNoeud().get(x), Double.MAX_VALUE);
+            valeurs.setParent(g.listeNoeud().get(x), "");
+        }
+        valeurs.setValeur(depart, 0);
+        valeurs.setParent(depart, "");
+        boolean modifie = true;
+        while(modifie) {
+            modifie = false;
+            for(int x = 0; x<g.listeNoeud().size(); x++) {
+                List<Arc> voisins = g.suivants(g.listeNoeud().get(x));
+                for(int n = 0; n<voisins.size(); n++) {
+                    double coutArc = voisins.get(n).getC();
+                    double nouvelleValeur = valeurs.getValeur(g.listeNoeud().get(x)) + coutArc;
+                    if(nouvelleValeur <= valeurs.getValeur(voisins.get(n).getD())) {
+                        valeurs.setValeur(voisins.get(n).getD(), nouvelleValeur);
+                        valeurs.setParent(voisins.get(n).getD(), g.listeNoeud().get(x));
+                        modifie = true;
+                    }
+                }
+            }
+        }
+        return valeurs;
     }
-
 
 }
